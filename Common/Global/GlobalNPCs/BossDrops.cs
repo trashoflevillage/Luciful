0677d3d3d89f8807914d61122446f966d2f944e8
trashoflevillage <uhhnokthxbye@gmail.com>
2022-10-30 13:53:16 -0400

    King Slime drops Pink Gel
    King Slime drops Pink Gel
    Deleted deprecated items

---
+++ Common/Global/GlobalNPCs/BossDrops.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.ItemDropRules;
+
+namespace Luciful.Common.Global.GlobalNPCs
+{
+    internal class BossDrops : GlobalNPC
+    {
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+        {
+            switch (npc.type)
+            {
+                case NPCID.KingSlime: npcLoot.Add(ItemDropRule.Common(ItemID.PinkGel, 1, 20, 35)); break;
+                default: break;
+            }
+        }
+    }
+}
