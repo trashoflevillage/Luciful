af1d35a723a3f3a85bbecd824ffddb95c2a557de
trashoflevillage <uhhnokthxbye@gmail.com>
2022-11-17 00:28:41 -0500

    Started Fractured Altar

---
+++ Content/Tiles/CraftingStations/FracturedAltarCorruption.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace Luciful.Content.Tiles.CraftingStations
+{
+	internal class FracturedAltarCorruption : ModTile
+	{
+		public override void SetStaticDefaults()
+		{
+			// Properties
+			Main.tileTable[Type] = false;
+			Main.tileSolidTop[Type] = false;
+			Main.tileNoAttach[Type] = true;
+			Main.tileLavaDeath[Type] = true;
+			Main.tileFrameImportant[Type] = true;
+			TileID.Sets.DisableSmartCursor[Type] = true;
+			TileID.Sets.IgnoredByNpcStepUp[Type] = true; // This line makes NPCs not try to step up this tile during their movement. Only use this for furniture with solid tops.
+
+			DustType = DustID.Corruption;
+			AdjTiles = new int[] { TileID.DemonAltar };
+
+			// Placement
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
+			TileObjectData.newTile.CoordinateHeights = new[] { 18 };
+			TileObjectData.addTile(Type);
+
+			AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
+
+			// Etc
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Fractured Altar");
+			AddMapEntry(new Color(255, 0, 255), name);
+		}
+
+		public override void NumDust(int x, int y, bool fail, ref int num)
+		{
+			num = fail ? 1 : 3;
+		}
+
+		public override void KillMultiTile(int x, int y, int frameX, int frameY)
+		{
+			Item.NewItem(new EntitySource_TileBreak(x, y), x * 16, y * 16, 32, 16, ModContent.ItemType<Items.Placeable.CraftingStations.FracturedAltar>());
+		}
+	}
+}
